1cc53463f1e18dd316884721e096bfc5a8ddca6c
Bharath Sai Muthyala <bharathmuthyala30@gmail.com>
2025-12-28 12:58:41 +0530

    Happy Birthday

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function () {
+  const WIDTH = window.innerWidth;
+  const HEIGHT = window.innerHeight;
+  const LYRICS_DELAY = 1500;
+  const MIN_WIDTH = 768;
+  const MIN_HEIGHT = 700;
+
+  console.log(
+    "The width and height are ",
+    WIDTH,
+    " and ",
+    HEIGHT,
+    " respectively"
+  );
+
+  if (WIDTH < MIN_WIDTH || HEIGHT < MIN_HEIGHT) {
+    alert("This is a small screen and not suitable for viewing");
+    if (confirm("Please use another larger screen?")) {
+      console.log("Another screen used");
+    } else {
+      console.log("they dont care");
+    }
+  } else {
+    const elements = document.querySelectorAll(".lyrics");
+
+    setTimeout(() => {
+      elements.forEach((element) => {
+        element.style.display = "block";
+      });
+    }, LYRICS_DELAY);
+  }
+});
